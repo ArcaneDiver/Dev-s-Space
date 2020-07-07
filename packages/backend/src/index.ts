fdd667b44917c32421b2f9e53dfb669c7d502553
ArcaneDiver <michele.dellamea@bearzi.org>
2020-07-07 13:17:36 +0200

    pipeline completed??

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
         console.log(`Server running at ${port} port`);
 })*/
-
+import "@ArcaneDiver/common"
 import { EventEmitter } from "events"
 import e = require("express");
 
